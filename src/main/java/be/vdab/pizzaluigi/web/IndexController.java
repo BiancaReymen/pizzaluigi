302e251f801ed69b1e4fe398231e10960be3d685
Dell <Dell@LAPTOP-DELL>
2018-10-12 09:10:15 +0200

    commit 9.4

--- src/main/java/be/vdab/pizzaluigi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluigi/web/IndexController.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 class IndexController {
 
 	@GetMapping
-	String index() {
-		return "/WEB-INF/JSP/index.jsp";
+	ModelAndView index() {
+		String boodschap;
+		int uur = LocalTime.now().getHour();
+		if (uur < 12) {
+			boodschap = "Goede morgen";
+		} else if (uur < 18) {
+			boodschap = "Goede middag";
+		} else {
+			boodschap = "Goede avond";
+		}
+		return new ModelAndView("/WEB-INF/JSP/index.jsp", "boodschap", boodschap);
 			
 	}
 		
--- src/main/webapp/WEB-INF/JSP/index.jsp
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html" pageEncoding="UTF-8" session="false"%>
 <!DOCTYPE html>
-<html lanf="nl">
+<html lang="nl">
 <head>
 <title>Pizza Luigi</title>
 <link rel='icon' href='images/pizza.ico' type='image/x-icon'>
@@ -11,6 +11,6 @@
 <body>
 	<h1>Pizza Luigi</h1>
 	<img src='images/pizza.jpg' alt='pizza' class='fullwidth'>
-	<h2>Goede dag</h2>
+	<h2>${boodschap}</h2>
 </body>
 </html>
