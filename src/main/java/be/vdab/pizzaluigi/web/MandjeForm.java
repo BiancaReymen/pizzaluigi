1eabe630ad4b909da22926b6b02cf7e702aa255b
Dell <Dell@192.168.2.12>
2018-11-12 10:59:12 +0100

    commit 36.6

---
+++ src/main/java/be/vdab/pizzaluigi/web/MandjeForm.java
@@ -0,0 +1,17 @@
+package be.vdab.pizzaluigi.web;
+
+class MandjeForm {
+	
+	private long pizzaId;
+
+	public long getPizzaId() {
+		return pizzaId;
+	}
+
+	public void setPizzaId(long pizzaId) {
+		this.pizzaId = pizzaId;
+	}
+	
+	
+
+}
